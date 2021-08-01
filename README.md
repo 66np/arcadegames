c23c665942c466647d034bfb1f6588605dd06372
neethap <37029408+neethap@users.noreply.github.com>
2021-07-31 21:07:06 -0400

    Adding Tetris game

--- README.md
+++ README.md
@@ -9,12 +9,17 @@ The source code from the following tutorials helped me build specific projects i
 - Snake: https://www.youtube.com/watch?v=7Azlj0f9vas&ab_channel=CodingWithAdam
 - Bejeweled: https://www.youtube.com/watch?v=XD5sZWxwJUk&t=412s&ab_channel=CodewithAniaKub%C3%B3w
 - Bejeweled Countdown: https://www.youtube.com/watch?v=vSV_Ml2_A88&ab_channel=CodewithAniaKub%C3%B3w
+- Tetris: https://www.youtube.com/watch?v=H2aW5V46khA&ab_channel=MethMethMethod
 
 ## Lessons Learned
 - Order of functions matter in JS file
 - Canvas is preferable for static and auto movement, while Div works better with event handlers (i.e. dragstart)
 - DOMContentLoaded is used to run event once HTML doc is loaded, but without waiting for stylesheets or images (https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event)
 - You can invoke a function once (i.e. to avoid tampering with a countdown) by using 'once' method in the event listener
+- Canvas scaling method, although convenient, often interferes with re-rendering of frames
+- requestAnimationFrame provides better quality animations over setInterval method
+- unshift array method adds items at the beginning of the array and returns the array's new length
+- debugger stops execution of JavaScript function and called function being debugged
 
 # Progress
 
--- tetris/tetris.js
+++ tetris/tetris.js
@@ -1,134 +1,290 @@
 /** @type {HTMLCanvasElement} */
 
-document.addEventListener("DOMContentLoaded", () => {
-    const canvas = document.getElementById("tetris");
-    const ctx = canvas.getContext("2d");
+const canvas = document.getElementById("tetris");
+const ctx = canvas.getContext("2d");
 
-    function clearScreen() {
-        ctx.fillStyle = '#fff200';
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
+canvas.width = 400;
+canvas.height = 400;
+
+// Scale of grid and tetris matrix
+const scaleFactor = 16;
+
+class drawScreen{
+    constructor(clr, xPos, yPos, boxWidth, boxHeight) {
+        this.clr = clr;
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.boxWidth = boxWidth;
+        this.boxHeight = boxHeight;
     }
 
-    clearScreen();
+    draw(context) {
+        context.fillStyle = this.clr;
+        context.fillRect(this.xPos, this.yPos, this.boxWidth, this.boxHeight);
+    }
+}
 
-    const grid = document.getElementById('grid-tetris');
-    let squares = Array.from(grid.querySelectorAll("#grid-block"));
-    const width = 13;
-    const height = 20;
-    let currentPosition = 4;
+function clearScreen() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
 
-    // Assign functions to keycodes
-    function control(event) {
-        if (event.keyCode === 39) {
-            moveRight();
-        } else if (event.keyCode === 38) {
-            rotate();
-        } else if (event.keyCode === 37) {
-            moveLeft();
-        } else if (event.keyCode === 40) {
-            moveDown();
+// Line thickness for Tetris game border
+const thickness = 1;
+
+// Tetris game dimensions
+const gameStartX = 0;
+const gameStartY = 0;
+const gameWidth = 224;
+const gameHeight = 352;
+
+
+function addedCanvasValuesToDrawScreen() {
+    // Clear screen
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Draw canvas background
+    let canvasBG = new drawScreen("#fff200", 0, 0, canvas.width, canvas.height);
+    canvasBG.draw(ctx);
+}
+
+function addBoxValuesToDrawScreen() {
+    // Draw tetris box
+    let boxBorder = new drawScreen("#000", gameStartX - thickness, gameStartY - thickness, gameWidth + thickness*2, gameHeight + thickness*2);
+    boxBorder.draw(ctx);
+
+    let boxFill = new drawScreen("#fff200", gameStartX, gameStartY, gameWidth, gameHeight);
+    boxFill.draw(ctx);
+
+}
+
+addedCanvasValuesToDrawScreen();
+addBoxValuesToDrawScreen();
+
+function arenaSweep() {
+    outer: for (let y = arena.length - 1; y > 0; --y) {
+        for (let x = 0; x < arena.length; ++x) {
+            if (arena[y][x] === 0) {
+                continue outer;
+            }
+        }
+
+        const row = arena.splice(y, 1)[0].fill(0);
+        arena.unshift(row);
+        ++y;
+    }
+}
+
+function collide(arena, player) {
+    const [m, o] = [player.matrix, player.pos];
+
+    for (let y = 0; y < m.length; ++y) {
+        for (let x = 0; x < m[y].length; ++x) {
+            if (m[y][x] !== 0 && (arena[y + o.y] && arena[y + o.y][x + o.x]) !== 0) {
+                return true;
+            }
         }
     }
+    return false;
+}
 
-    document.addEventListener('keyup', control);
+function createMatrix(w, h) {
+    const matrix = [];
 
-    // Defining Tetramino shapes
+    while (h--) {
+        matrix.push(new Array(w).fill(0));
+    }
 
-    const lTetromino = [
-        [1, width+1, width*2+1, 2],
-        [width, width+1, width+2, width*2+2],
-        [1, width+1, width*2+1, width*2],
-        [width, width*2, width*2+1, width*2+2]
-    ];
+    return matrix;
+}
 
-    const zTetromino = [
-        [0, width, width+1, width*2+1],
-        [width+1, width+2, width*2, width*2+1],
-        [0, width, width+1, width*2+1],
-        [width+1, width+2, width*2, width*2+1]
-    ];
+function createPiece(type) {
+    if (type === "T") {
+        return [
+            [0, 0, 0],
+            [1, 1, 1],
+            [0, 1, 0],
+        ];
+    } else if (type === "O") {
+        return [
+            [2, 2],
+            [2, 2],
+        ];
+    } else if (type === "L") {
+        return [
+            [0, 3, 0],
+            [0, 3, 0],
+            [0, 3, 3],
+        ];
+    } else if (type === "J") {
+        return [
+            [0, 4, 0],
+            [0, 4, 0],
+            [4, 4, 0],
+        ];
+    } else if (type === "I") {
+        return [
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+        ];
+    } else if (type === "S") {
+        return [
+            [0, 6, 6],
+            [6, 6, 0],
+            [0, 0, 0],
+        ];
+    } else if (type === "Z") {
+        return [
+            [7, 7, 0],
+            [0, 7, 7],
+            [0, 0, 0],
+        ];
+    }
+}
 
-    const tTetromino = [
-        [1, width, width+1, width+2],
-        [1, width+1, width+2, width*2+1],
-        [width, width+1, width+2, width*2+1],
-        [1, width, width+1, width*2+1]
-    ];
+function draw() {
+    addedCanvasValuesToDrawScreen();
+    addBoxValuesToDrawScreen();
 
-    const oTetromino = [
-        [0, 1, width, width+1],
-        [0, 1, width, width+1],
-        [0, 1, width, width+1],
-        [0, 1, width, width+1]
-    ];
+    drawMatrix(arena, {x:0, y: 0});
 
-    const iTetromino = [
-        [1, width+1, width*2+1, width*3+1],
-        [width, width+1, width+2, width+3],
-        [1, width+1, width*2+1, width*3+1],
-        [width, width+1, width+2, width+3]
-    ];
+    drawMatrix(player.matrix, player.pos);
+}
 
-    const theTetrominos = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+function drawMatrix(matrix, offset) {
+    matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                let matrixBlock = new drawScreen(colors[value], (x + offset.x)*scaleFactor, (y + offset.y)*scaleFactor, (1)*scaleFactor, (1)*scaleFactor);
+                matrixBlock.draw(ctx);
+            }
+        });
+    });
+}
 
-    // Random selection of Tetromino
-    let random = Math.floor(Math.random()*theTetrominos.length);
-    let currentRotation = 0;
-    let current = theTetrominos[random][currentRotation];
-
-    // Draw the shape
-    function draw() {
-        current.forEach(index => {
-            squares[currentPosition + index].classList.add('tetris-block');
+function merge(arena, player) {
+    player.matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                arena[y + player.pos.y][x + player.pos.x] = value;
+            }
         })
-    }
+    })
+}
 
-    // Undraw the shape
-    function undraw() {
-        current.forEach(index => {
-            squares[currentPosition + index].classList.remove('tetris-block');
-        })
+function playerDrop() {
+    player.pos.y++;
+    if (collide(arena, player)) {
+        player.pos.y--;
+        merge(arena, player);
+        playerReset();
+        arenaSweep();
     }
+    dropCounter = 0;
+}
 
-    // Move down shape
-    function moveDown() {
-        undraw();
-        currentPosition = currentPosition += width;
-        draw();
-        freeze();
+function playerMove(dir) {
+    player.pos.x += dir;
+    if (collide(arena, player)) {
+        player.pos.x -= dir;
     }
+}
 
-    // Move left and prevent collisions with shapes moving left
-    function moveRight() {
-        undraw();
-        const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1);
-        if (!isAtRightEdge) currentPosition += 1;
-        if (current.some(index => squares[currentPosition + index].classList.contains("tetris-block2"))) {
-            currentPosition -= 1;
+function playerReset() {
+    const pieces = "ILJOTSZ";
+    player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
+    player.pos.y = 0;
+    player.pos.x = (arena[0].length / 2 | 0) - (player.matrix[0].length / 2 | 0); 
+    if (collide(arena, player)) {
+        arena.forEach(row => row.fill(0));
+    }
+}
+
+function playerRotate(dir) {
+    const pos = player.pos.x;
+    let offset = 1;
+    rotate(player.matrix, dir);
+    while (collide(arena, player)) {
+        player.pos.x += offset;
+        offset = - (offset + (offset > 0 ? 1 : -1));
+        if (offset > player.matrix[0].length) {
+            rotate(player.matrix, - dir);
+            player.pos.x = pos;
+            return;
         }
-        draw();
+    }
+}
+
+function rotate(matrix, dir) {
+    for (let y = 0; y < matrix.length; ++y) {
+        for (let x = 0; x < y; ++x) {
+            [
+                matrix[x][y],
+                matrix[y][x],
+            ] = [
+                matrix[y][x],
+                matrix[x][y],
+            ]
+        }
     }
 
-    function moveLeft() {
-        undraw();
-        const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
-        if (!isAtLeftEdge) currentPosition -= 1;
-        if(current.some(index =>  squares[currentPosition + index].classList.contains("tetris-block2"))) {
-            currentPosition += 1;
-        }
-        draw();
+    if (dir >  0) {
+        matrix.forEach(row => row.reverse());
+    } else {
+        matrix.reverse();
+    }
+}
+
+// In milliseconds
+let dropCounter = 0;
+let dropInterval = 1000;
+
+let lastTime = 0;
+
+function update(time = 0) {
+    const deltaTime = time - lastTime;
+    lastTime = time;
+
+    dropCounter += deltaTime;
+    if (dropCounter > dropInterval) {
+        playerDrop();
     }
 
-    // Rotate Tetromino
-    function rotate() {
-        undraw();
-        currentRotation++;
-        if (currentRotation === current.length) {
-            currentRotation = 0;
-        }
-        current = theTetrominos[random][currentRotation];
-        draw();
-    }
-    
     draw();
+    requestAnimationFrame(update);
+};
+
+const colors = [
+    null,
+    'purple',
+    'red',
+    'green',
+    'blue',
+    'hotpink',
+    'darkgoldenrod',
+    'rebeccapurple'
+]
+
+const arena = createMatrix(14, 22);
+
+const player = {
+    pos: {x: 1, y: 2},
+    matrix: createPiece("T"),
+};
+
+document.addEventListener("keydown", event => {
+    if (event.key === "ArrowLeft") { // keyCode = 37
+        playerMove(-1);
+    } else if (event.key === "ArrowRight") { // keyCode = 39
+        playerMove(1);
+    } else if (event.key === "ArrowDown") { // keyCode = 40
+        playerDrop();
+    } else if (event.key === "ArrowUp") { // keyCode = 38
+        playerRotate(1);
+    }
 })
+
+document.onkeyup = function() {
+    update();
+}
